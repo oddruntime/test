46763bc591a6817f012ddf3e41fd1e40093099ad
oddruntime <66037774+oddruntime@users.noreply.github.com>
2025-02-10 18:47:10 +0100

    Create srv.py

---
+++ srv.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer as BaseHTTPServer, SimpleHTTPRequestHandler
+import os
+
+
+class HTTPHandler(SimpleHTTPRequestHandler):
+    """This handler uses server.base_path instead of always using os.getcwd()"""
+
+    def translate_path(self, path):
+        path = SimpleHTTPRequestHandler.translate_path(self, path)
+        relpath = os.path.relpath(path, os.getcwd())
+        fullpath = os.path.join(self.server.base_path, relpath)
+        return fullpath
+
+
+class HTTPServer(BaseHTTPServer):
+    """The main server, you pass in base_path which is the path you want to serve requests from"""
+
+    def __init__(self, base_path, server_address, RequestHandlerClass=HTTPHandler):
+        self.base_path = base_path
+        BaseHTTPServer.__init__(self, server_address, RequestHandlerClass)
+
+
+web_dir = os.path.join(os.path.dirname(__file__), 'imbue')
+httpd = HTTPServer(web_dir, ("", 12376))
+httpd.serve_forever()
